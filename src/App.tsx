7412b9089f774177a9b1d27e2a308d333286c2bf
Samuel Shear <dspfgh34@gmail.com>
2021-08-02 19:26:10 -0400

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sass/App.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+        </div>
+    );
+}
+
+export default App;
