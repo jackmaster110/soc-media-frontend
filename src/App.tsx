8a56afccb332faf01330512800058c2ea05e644f
Samuel Shear <dspfgh34@gmail.com>
2021-08-03 19:41:12 -0400

    Installed dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import "./sass/App.scss";
 
 function App() {
     return (
-        <div className="App">
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
+        <div className="app-container">
+            <header className="app-header">
+                <h1>Soc Media</h1>
+            </header>
         </div>
     );
 }
